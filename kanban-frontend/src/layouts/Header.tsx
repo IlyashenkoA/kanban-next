ce84d32ace2ca6642967ee0f40bd49c9ca3cd2df
Arthur Ilyashenko <ilyashenko.arthur@gmail.com>
2023-11-09 14:26:46 +0200

    change logout color

--- kanban-frontend/src/layouts/Header.tsx
+++ kanban-frontend/src/layouts/Header.tsx
@@ -80,7 +80,7 @@ export const Header: React.FC<{
                 viewBox='0 0 64 64'
               >
                 <path
-                  className='dark:fill-light-bg fill-dark-bg'
+                  className='dark:fill-medium-grey fill-dark-bg'
                   fill-rule='evenodd'
                   d='M10.666 12a6.667 6.667 0 0 1 6.667-6.667h29.334A6.667 6.667 0 0 1 53.332 12v7.333a1.333 1.333 0 0 1-2.666 0V12a4 4 0 0 0-4-4H17.332a4 4 0 0 0-4 4v40a4 4 0 0 0 4 4h29.334a4 4 0 0 0 4-4v-7.334a1.333 1.333 0 0 1 2.666 0V52a6.667 6.667 0 0 1-6.666 6.666H17.332A6.667 6.667 0 0 1 10.667 52V12Z'
                   clip-rule='evenodd'
